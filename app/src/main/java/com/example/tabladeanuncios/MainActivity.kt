62e167220993dbbbf8687f638fa5fb8e81da8464
Nobilis <nikolaev-evgeny@yandex.ru>
2023-02-24 15:34:19 +0300

    2023-02-24/15.29 ViewBinding Замена kotlinx.synthetics на BindingView. Замена OnNavigationItemSelectedListener (deprecated) на OnItemSelectedListener

--- app/src/main/java/com/example/tabladeanuncios/MainActivity.kt
+++ app/src/main/java/com/example/tabladeanuncios/MainActivity.kt
@@ -1,33 +1,35 @@
 package com.example.tabladeanuncios
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import com.example.tabladeanuncios.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener,
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener,
     NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init() {
         val toggle = ActionBarDrawerToggle(this,
-            drawerLayout,
-            toolbar,
+            binding.drawerLayout,
+            binding.mainContent.toolbar,
             R.string.open,
             R.string.close)
-        drawerLayout.addDrawerListener(toggle)
+        binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        navView.setNavigationItemSelectedListener(this)
+        binding.navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -65,7 +67,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener,
                 //TODO
             }
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
