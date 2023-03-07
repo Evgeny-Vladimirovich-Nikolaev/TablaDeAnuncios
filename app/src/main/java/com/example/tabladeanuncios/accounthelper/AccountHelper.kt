9cc3ae2f8b15df83fc7d22c47c0c440aca5f1761
Nobilis <nikolaev-evgeny@yandex.ru>
2023-03-07 21:39:58 +0300

    2023-03-08/00.39 Реализована функция регистрации.

--- app/src/main/java/com/example/tabladeanuncios/accounthelper/AccountHelper.kt
+++ app/src/main/java/com/example/tabladeanuncios/accounthelper/AccountHelper.kt
@@ -3,6 +3,7 @@ package com.example.tabladeanuncios.accounthelper
 import android.widget.Toast
 import com.example.tabladeanuncios.MainActivity
 import com.example.tabladeanuncios.R
+import com.google.firebase.auth.FirebaseUser
 
 class AccountHelper(act: MainActivity) {
 
@@ -13,13 +14,27 @@ class AccountHelper(act: MainActivity) {
             act.mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-
+                        sendEmailVerification(task.result?.user!!)
                     } else {
                         Toast
                             .makeText(act, act.resources.getString(R.string.sign_up_error), Toast.LENGTH_LONG)
                             .show()
                     }
                 }
+        }
+    }
+
+    private fun sendEmailVerification(user: FirebaseUser) {
+        user.sendEmailVerification().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast
+                    .makeText(act, act.resources.getString(R.string.send_verification_done), Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                Toast
+                    .makeText(act, act.resources.getString(R.string.send_verification_error), Toast.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/tabladeanuncios/dialoghelper/DialogHelper.kt
+++ app/src/main/java/com/example/tabladeanuncios/dialoghelper/DialogHelper.kt
@@ -3,10 +3,12 @@ package com.example.tabladeanuncios.dialoghelper
 import android.app.AlertDialog
 import com.example.tabladeanuncios.MainActivity
 import com.example.tabladeanuncios.R
+import com.example.tabladeanuncios.accounthelper.AccountHelper
 import com.example.tabladeanuncios.databinding.SignDialogBinding
 
 class DialogHelper(act: MainActivity) {
     private val act = act
+    private val accHelper = AccountHelper(act)
     fun createSignDialog(index: Int) {
         val builder = AlertDialog.Builder(act)
         val binding = SignDialogBinding.inflate(act.layoutInflater)
@@ -16,6 +18,13 @@ class DialogHelper(act: MainActivity) {
         } else {
             binding.tvSignTitle.text = act.resources.getText(R.string.ad_sign_in)
             binding.btSignUpIn.text = act.resources.getText(R.string.sing_in_action)
+        }
+        binding.btSignUpIn.setOnClickListener {
+            if(index == DialogConst.SIGN_UP_STATE) {
+                accHelper.signUpWithEmail(
+                    binding.edSignEmail.text.toString(),
+                    binding.edSignPassword.text.toString())
+            }
         }
         builder.setView(binding.root)
         builder.show()
